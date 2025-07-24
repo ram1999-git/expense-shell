749198d17a5d692915e455d5eb2c7d4a40e54b56
Ram Chaithanya <ramchaithanya1999@.com>
2025-07-24 11:52:23 +0530

    Calling other script

---
+++ call other script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+course="Devops from current script"
+
+echo "Before calling other script course:$course"
+echo "Process instance id of current script:$$"
+
+./normal calling script.sh
+
+echo "After calling other script course:$course"
+echo "Process instance id of current script:$$"
---
+++ normal calling script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+course="Devops from other script"
+
+echo "variable value from other script course:$course"
+echo "Process instance id of current script:$$"
