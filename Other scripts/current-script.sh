bfa05a863109e84bc6751a7221b57073b7f79f45
Ram Chaithanya <ramchaithanya1999@.com>
2025-07-25 10:13:51 +0530

    This is a testing of calling other script

---
+++ Other scripts/current-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+course="Devops from current script"
+
+echo "Before calling script, Course:$course"
+echo "Process Instance ID of the script is:$$"
+
+./Other-script.sh
+
+echo "After calling script,course:$course
--- normal calling script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-course="Devops from other script"
-
-echo "variable value from other script course:$course"
-echo "Process instance id of current script:$$"
